a429b09a43df354688217ed174b53467b06d00a2
wizard503 <josemdg12@gmail.com>
2023-04-22 12:34:21 -0600

    agregando componente

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   import HomeImages from "./components/HomeImages.vue";
   import Navbar from "./components/Navbar.vue";
   import AboutUs from "./components/AboutUs.vue";
+  import Services from "./components/Servicios.vue";
   import FooterContainer from "./components/FooterContainer.vue";
   import SocialNetwork from "./components/SocialNetwork.vue";
   import Projects from "./components/Projects.vue";
@@ -12,6 +13,7 @@
   <HomeImages />
   <SocialNetwork />
   <AboutUs/>
+  <Services/>
   <Projects />
   <FooterContainer />
 </template>
