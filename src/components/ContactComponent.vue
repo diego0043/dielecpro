f94e468d92d7c87dcf0b437688a5208e032c5041
Diego Valencia <ve18007@ues.edu.sv>
2023-04-22 21:29:44 -0600

    Solo falta parte de contacto en footer

---
+++ src/components/ContactComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container-fluid container-social">
+    <a href=""></a>
+  </div>
+</template>
+
+<style>
+.container-social{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 5px;
+}
+</style>
