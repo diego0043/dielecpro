e2429925ae6e6352f7d4059d557e313d4b625e03
wizard503 <josemdg12@gmail.com>
2023-04-22 21:59:53 -0600

    iniciando mision y vision

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -27,6 +27,13 @@
 			</div>
 		</div>
 	</div>
+	<div class="row">
+		<div class="col-sm-6 col-md-3"></div>
+		<div class="col-sm-6 col-md-3"></div>
+		<div class="col-sm-6 col-md-3"></div>
+		<div class="col-sm-6 col-md-3"></div>
+
+	</div>
 </div>
 </template>
 
