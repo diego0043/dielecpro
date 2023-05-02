6bb209194f1c39c5c89271250328abb749bafee5
wizard503 <josemdg12@gmail.com>
2023-05-01 21:42:47 -0600

    optimizando css y documentando el codigo

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,15 +1,25 @@
 <template>
-<div class="container pt-2">
+<!--Contenedor principal con paddind top de 2-->
+<div data-aos="fade-up" data-aos-duration="1800" class="container pt-2">
+	<!--Fila con padding top de 5 y bottom de 4-->
 	<div class="row pt-5 pb-4">
+		<!--Columna de 7 de ancho en pantallas pequeñas y 5 en medianas -->
 		<div class="col-sm-7 col-md-5">
+			<!--Imagenes con su id uno y dos respectivamente-->
 			<img id="uno" src="https://media.istockphoto.com/id/1268557129/es/foto/electricistas-cableado-de-una-nueva-construcci%C3%B3n.jpg?s=612x612&w=0&k=20&c=oSTD7KqJ_rccB7nPvJUQhxkoVpSa4SVJknGwyYrsNDE=" alt="...">
 			<img id="dos" src="https://img.freepik.com/fotos-premium/electricistas-trabajan-conectar-cables-electricos-sistema-tablero-distribucion-sistema-electrico-gabinete-control_545582-66.jpg?w=740" alt="">
 		</div>
+		<!--Columna de 12 de ancho en pantallas pequeñas y 7 en medianas-->
 		<div class="col-sm-12 col-md-7">
+			<!--Contenedor de texto con la descripcion de about us-->
 			<div class="about">
+				<!--Titulo sobre nosotros-->
 				<h6>Sobre nosotros</h6>
+				<!--Titulo principal con tamaño de 4-->
 				<h4>¡Somos la empresa de referencia en el mercado! con más de una década de experiencia</h4>
+				<!--Parrafo descriptivo sobre nosotros-->
 				<p>Nos hemos consolidado como líderes indiscutibles en la ejecución exitosa de proyectos de media y baja tensión, así como en el sector industrial y comercial en todo el territorio nacional, ponemos a su disposicion: </p>
+				<!--Lista con 4 elementos principales sobre nosotros-->
 				<ul>
 					<li>
 						Equipo de expertos altamente calificados con una gran capacidad para abordar proyectos de cualquier envergadura.
@@ -24,39 +34,53 @@
 						Nuestro compromiso a superar tus expectativas en cada proyecto que llevamos a cabo, y sobre todo la capacidad para entregar resultados sobresalientes.
 					</li>
 				</ul>
+				<!--Titulo con tamaño de 5-->
 				<h5>¡Confía en nosotros para llevar tu idea al siguiente nivel!</h5>
 			</div>
 		</div>
 	</div>
 </div>
+<!--Contenedor de secciones de Mision y Vision para vista movil y tableta grande-->
 <div class="misionvision-uno">
+		<!--Fila con margin top,bottom y padding bottom de 5-->
 		<div class="row mt-5 pb-5 mb-5">
-			<div class="mision1-bg">			
+			<!--Div de mision con background-->
+			<div class="mision1-bg">
+				<!--Descripcion de la mision-->			
 				<div class="mision1">
 					<h4 class="mision1-title">MISION</h4>
-					<p class="mision1-desciption">Nuestra misión es liderar el mercado ejecutando proyectos de media y baja tensión, así como en el ámbito industrial y comercial a nivel nacional, con soluciones innovadoras y eficientes respaldadas por nuestra experiencia de 10 años. Nos comprometemos con la calidad, cumplimiento de plazos y satisfacción del cliente. Buscamos establecer relaciones duraderas basadas en la confianza, excelencia y ser la opción preferida en el mercado.</p>
+					<p class="mision1-description">Nuestra misión es liderar el mercado ejecutando proyectos de media y baja tensión, así como en el ámbito industrial y comercial a nivel nacional, con soluciones innovadoras y eficientes respaldadas por nuestra experiencia de 10 años. Nos comprometemos con la calidad, cumplimiento de plazos y satisfacción del cliente. Buscamos establecer relaciones duraderas basadas en la confianza, excelencia y ser la opción preferida en el mercado.</p>
 				</div>
 			</div>
 		</div>
+		<!--Fila con margin top, top y padding bottom de 5-->
 		<div class="row mt-5 pb-5 mb-5">
+			<!--Div de vision con background-->
 			<div class="vision1-bg">
+				<!--Descripcion de la vision-->
 				<div class="vision1">
 					<h4 class="vision1-title">VISION</h4>
-					<p class="vision1-desciption">Nuestra misión es liderar el mercado ejecutando proyectos de media y baja tensión, así como en el ámbito industrial y comercial a nivel nacional, con soluciones innovadoras y eficientes respaldadas por nuestra experiencia de 10 años. Nos comprometemos con la calidad, cumplimiento de plazos y satisfacción del cliente. Buscamos establecer relaciones duraderas basadas en la confianza, excelencia y ser la opción preferida en el mercado.</p>
+					<p class="vision1-description">Nuestra misión es liderar el mercado ejecutando proyectos de media y baja tensión, así como en el ámbito industrial y comercial a nivel nacional, con soluciones innovadoras y eficientes respaldadas por nuestra experiencia de 10 años. Nos comprometemos con la calidad, cumplimiento de plazos y satisfacción del cliente. Buscamos establecer relaciones duraderas basadas en la confianza, excelencia y ser la opción preferida en el mercado.</p>
 				</div>
 			</div>
 		</div>
 </div>
-<div class="mision-dos mt-4">
+<!--Contenedor de seccion Mision para vista PC-->
+<div data-aos="fade-right" data-aos-duration="1700" class="mision-dos mt-4">
+	<!--Columna de 10 de ancho en pantallas pequeñas y 6 en medianas con margin bottom de 5-->
 	<div class="col-sm-10 col-md-6 mb-5 ">
+		<!--Div contenedor de la descripcion de la mision-->
 		<div class="mision2" >
 			<h4 class="mision2-title">MISION</h4>
-			<p class="mision2-desciption">Nuestra misión es liderar el mercado ejecutando proyectos de media y baja tensión, así como en el ámbito industrial y comercial a nivel nacional, con soluciones innovadoras y eficientes respaldadas por nuestra experiencia de 10 años. Nos comprometemos con la calidad, cumplimiento de plazos y satisfacción del cliente. Buscamos establecer relaciones duraderas basadas en la confianza, excelencia y ser la opción preferida en el mercado.</p>
+			<p class="mision2-description">Nuestra misión es liderar el mercado ejecutando proyectos de media y baja tensión, así como en el ámbito industrial y comercial a nivel nacional, con soluciones innovadoras y eficientes respaldadas por nuestra experiencia de 10 años. Nos comprometemos con la calidad, cumplimiento de plazos y satisfacción del cliente. Buscamos establecer relaciones duraderas basadas en la confianza, excelencia y ser la opción preferida en el mercado.</p>
 		</div>
 	</div>
 </div>
-<div class="vision-dos mt-4">
+<!--Contenedor de seccion Vision para vista PC-->
+<div data-aos="fade-left" data-aos-duration="1700" class="vision-dos mt-4">
+	<!--Columna de 10 de ancho en pantallas pequeñas y 6 en medianas con margin bottom de 5-->
 	<div class="col-sm-10 col-md-6 mb-5">
+		<!--Div contenedor de la descripcion de la vision-->
 		<div class="vision2" >
 			<h4 class="vision2-title">VISION</h4>
 			<p class="vision2-description">Nuestra visión es ser reconocidos como una empresa innovadora y eficiente, que se destaca por su amplia experiencia en el sector. Buscamos ser la opción preferida de nuestros clientes, ofreciendo soluciones de vanguardia respaldadas por nuestro equipo altamente calificado y actualizados con las últimas tendencias tecnológicas. Aspiramos a mantener relaciones duraderas con nuestros clientes, basadas en la confianza, la calidad, el cumplimiento y la excelencia en cada proyecto que emprendemos.</p>
@@ -72,11 +96,10 @@
 			<div class="intro">
 				<h3>Papá de Navas</h3>
 				<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat distinctio dolorum animi adipisci magni nisi eveniet numquam iusto quo quod odio consectetur debitis officia ducimus ipsa quas, ipsum nam maxime.</p>
-			</div>
+				</div>
 		</div>
 	</div>
-  <Accordion />
-
+	<Accordion />
 </div>
 
 </template>
@@ -166,11 +189,14 @@ import Accordion from "./Accordion.vue"
 	opacity: 0;
 }
 
-/* Sobre nosotros*/
+/* Estilos para la sección "Sobre nosotros" */
+
+/* Estilos para el primer elemento con id="uno" */
 #uno{
 	width: 130%;
-
 }
+
+/* Estilos para el segundo elemento con id="dos" */
 #dos{
 	width: 75%;
 	position: relative;
@@ -178,6 +204,8 @@ import Accordion from "./Accordion.vue"
 	bottom: 25%;
 	left: -10%;
 }
+
+/* Estilos para el contenedor con la clase "about" */
 .about{
 	height: 90%;
 	top: 5%;
@@ -188,30 +216,38 @@ import Accordion from "./Accordion.vue"
 	text-align: justify;
 }
 
+/* Estilos para el elemento "h6" dentro del contenedor con la clase "about" */
 .about h6{
 	color: var(--color-primary);
 	font-weight: 700;
 	font-size: 18px;
 }
 
-.about h4,h5{
+/* Estilos para los elementos "h4" y "h5" dentro del contenedor con la clase "about" */
+.about h4,
+h5{
 	color: var(--text-primary);
 	font-weight: 900;
 }
 
-.about p,ul{
+/* Estilos para los elementos "p" y "ul" dentro del contenedor con la clase "about" */
+.about p,
+ul{
 	padding-top: 2%;
 	line-height: 30px;
 }
 
+/* Estilos para los elementos "ul" dentro del contenedor con la clase "container" */
 .container ul{
-	list-style: none;
+	list-style: none; /*Elimina el estilo de viñetas predeterminado de las listas*/
 }
 
+/* Estilos para los elementos "li" dentro del contenedor con la clase "container" */
 .container li::before{
 	content: '⚡';
 }
 
+/* Estilos para pantallas con un ancho máximo de 900px */
 @media (max-width: 900px) {
 	#dos{
 		bottom: 14%;
@@ -226,6 +262,7 @@ import Accordion from "./Accordion.vue"
 	}
 }
 
+/* Estilos para pantallas con un ancho máximo de 800px */
 @media (max-width: 800px) {
 	.about{
 		top: 5%;
@@ -235,7 +272,7 @@ import Accordion from "./Accordion.vue"
 		bottom: 15%;
 	}
 }
-
+/* Estilos para pantallas con un ancho máximo de 700px */
 @media (max-width: 700px) {
 	#uno{
 		width: 90%;
@@ -255,34 +292,38 @@ import Accordion from "./Accordion.vue"
 	}
 }
 
-/* Mision y Vision*/
-
-.mision1 .mision1-title,.mision2 .mision2-title ,.vision2 .vision2-title {
-    font-size: 36px;
-    line-height: 46px;
+/* Estilos para los títulos de Mision y Vision tanto uno como dos*/
+.mision1-title,
+.vision1-title,
+.mision2-title,
+.vision2-title {
+	font:600 36px/46px "Poppins", sans-serif;
     color: var(--text-primary);
-	font-weight: 600;
-    font-family: "Poppins", sans-serif;
     position: relative;
+    margin: 0 0 23px 0;
+}
+
+/* Padding para los titulos de mision y vision 2 y mision 1 */
+.mision1-title,
+.mision2-title,
+.vision2-title {
     padding: 37px 0 20px 20%;
-    margin: 0 0 23px 0;
 }
 
-.vision1 .vision1-title{
-	font-size: 36px;
-    line-height: 46px;
-    color: var(--text-primary);
-	font-weight: 600;
-    font-family: "Poppins", sans-serif;
-    position: relative;
+/* Estilos para el título de visión 1 */
+.vision1-title{
     padding: 37px 0 20px 30%;
-    margin: 0 0 23px 0;
 }
 
-.vision2 .vision2-title,.vision2 .vision2-description{
+/* Estilos para el titulo y la descripción de Visión 2 */
+.vision2-title,
+.vision2-description{
 	text-align: right;
 }
-.mision1 .mision1-title::before{
+
+/* Estilos para la línea vertical en el título de Mision y Vision 1*/
+.mision1-title::before,
+.vision1-title::before{
     content: '';
     width: 8px;
     height: calc(100% - 8px);
@@ -290,100 +331,117 @@ import Accordion from "./Accordion.vue"
     position: absolute;
     top: 5px;
     left: 0;
-}
-.vision1 .vision1-title::before{
-	content: '';
-    width: 8px;
-    height: calc(100% - 8px);
-    background-color: var(--color-secondary);
-    position: absolute;
-    top: 5px;
+}	
+
+/* Modificando posicion de la línea vertical en el título de Misión 1 */
+.mision1-title::before{
+    left: 0;
+}	
+
+/* Modificando posicion de la línea vertical en el título de Visión 1 */
+.vision1-title::before{
     left: 95%;
 }
-.mision1,.vision1,.mision2,.vision2{
+
+/* Estilos para la sección de Mision 1, Visión 1, Mision 2 y Visión 2 */
+.mision1,
+.vision1,
+.mision2,
+.vision2{
 	padding: 10%;
 	position: relative;
 	background: #fff;
 	font-family: 'Poppins', sans-serif;
 }
 
+/* Estilos para la sección de Mision 2 */
 .mision2 {
 	border-radius: 0% 53% 0% 0% / 8% 69% 0% 0%;
 }
 
+/* Estilos para la sección de Visión 2 */
 .vision2 {
 	right: -100%;
 	border-radius: 69% 0% 0% 8% / 83% 25% 25% 0%;
 }
 
+/* Estilos para la sombra en la sección de Mision 1 */
 .mision1{
 	box-shadow: 5px 5px 15px 1px rgba(0,0,0, 0.5);
 }
 
+/* Estilos para la sombra en la sección de Visión 1 */
 .vision1{
 	box-shadow: -5px 5px 15px 1px rgba(0,0,0, 0.5);
 }
 
-.mision1-bg{
-	padding: 10%;
-	width: 100%;
-	top: -10%;
-	position: relative;
-	background-image: url('https://i.ibb.co/FVtVbkC/d6f811fd0c668b06401e12f22b91eabb-XL1.jpg');	
-	background-repeat: no-repeat;
-	background-size: cover;
-}
-
+/* Estilos para el fondo de la sección de Mision y Vision 1*/
+.mision1-bg,
 .vision1-bg{
 	padding: 10%;
 	width: 100%;
 	top: -10%;
-	background-image: url('https://i.ibb.co/FVtVbkC/d6f811fd0c668b06401e12f22b91eabb-XL2.jpg');	
 	background-repeat: no-repeat;
 	background-size: cover;
 }
+
+/* Agregando el fondo de la sección de Mision 1 */
+.mision1-bg{
+	position: relative;
+	background-image: url('https://i.ibb.co/5sm7vSN/d6f811fd0c668b06401e12f22b91eabb-XL1.jpg');	
+}
+
+/* Agregando el fondo de la sección de Vision 1 */
+.vision1-bg{
+	background-image: url('https://i.ibb.co/FVtVbkC/d6f811fd0c668b06401e12f22b91eabb-XL2.jpg');	
+}
+
+/* Estilos para el fondo de la sección de Mision y Vision 2 */
+.mision-dos,
+.vision-dos{
+	background-repeat: no-repeat;
+	background-size: cover;
+}
+
+/* Agregando el fondo de la sección de Mision 2*/
 .mision-dos{
 	background-image: url('https://sonarverde.files.wordpress.com/2014/01/banner-electricidad.jpg');	
-	background-repeat: no-repeat;
-	background-size: cover;
 }
 
+/* Agregando el fondo de la sección de Vision 2 */
 .vision-dos{
 	background-image: url('https://cdn.euroinnova.edu.es/euroinnova_es/img_destacados/l/Curso-Superior-Intervenciones-Mantenimiento-Instalaciones-Electricas-Edificios.jpg');	
-	background-repeat: no-repeat;
-	background-size: cover;
 }
 
+/* Estilos para pantallas con un ancho máximo de 1600px */
 @media (max-width: 1600px) {
 	.misionvision-uno{
 		display: none;
 	}
-	.mision-dos,.vision-dos{
+	.mision-dos,
+	.vision-dos {
 		display: block;
 	}
 }
+
+/* Estilos para pantallas con un ancho máximo de 755px */
 @media (max-width: 775px) {
 
 	.misionvision-uno{
 		display: contents;
 	}
-	.mision-dos,.vision-dos{
+	.mision-dos,
+	.vision-dos {
 		display: none;
 	}
-	.mision1 {
-		display: block;
-		margin-left: auto;
-		margin-right: auto;
-		width: 100%;
-	}
+	.mision1,
 	.vision1 {
 		display: block;
 		margin-left: auto;
 		margin-right: auto;
 		width: 100%;
 	}
-
-	.mision1-desciption{
+	.mision1-description{
 		text-align: left;
 	}
 }
